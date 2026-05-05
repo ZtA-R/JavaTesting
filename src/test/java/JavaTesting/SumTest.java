8886e03b0b9a17fdae4f3e4f8e7c92fa61ca1896
Gabi <virussz056@gmail.com>
2026-05-05 12:58:32 +0200

    Resultado comprobado = Esta correcto

--- src/test/java/JavaTesting/SumTest.java
+++ src/test/java/JavaTesting/SumTest.java
@@ -1,10 +1,14 @@
 package JavaTesting;
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SumTest {
 
     @Test
     public void sumTest(){
+        int r = 1 + 1;
+        assertEquals(2, r , "1 + 1 = 2");
 
     }
 }
