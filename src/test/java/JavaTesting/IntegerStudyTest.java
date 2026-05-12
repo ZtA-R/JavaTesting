668ab83b4134b3c1f08cb4d67165e4013d7d8787
Gabi <virussz056@gmail.com>
2026-05-12 12:19:43 +0200

    Ejercicio 3 Resultado comprobado = Correcto

---
+++ src/test/java/JavaTesting/IntegerStudyTest.java
@@ -0,0 +1,61 @@
+package JavaTesting;
+
+import org.junit.Test;
+
+import javax.swing.tree.FixedHeightLayoutCache;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class IntegerStudyTest {
+
+    @Test
+    public void integerStudy(){
+
+            Integer vuit = new Integer(8);
+            Integer vint = new Integer("20");
+            Integer tretze = 13;
+            int esperat = 13;
+
+            int minim = -2147483648;
+
+            assertEquals(" El nombre tretze val realmente 13 ", esperat , tretze.intValue());
+
+            assertEquals(" El nombre vint val realmente 20 ", 20 , vint.intValue());
+
+            assertEquals(" El valor minim  d'un int es -2147483648", minim , Integer.MIN_VALUE  );
+
+    }
+
+    @Test
+    public void integerComparatStudy(){
+
+        Integer vuit = new Integer(8);
+        Integer eight = new Integer("8");
+
+        assertEquals(" El nombre vuit val realmente 8 ", 8 , vuit.intValue());
+
+        assertEquals(" El nombre eight val realmente 8 ", 8 , eight.intValue());
+
+        //assertTrue( vuit == eight );
+
+        assertTrue( vuit.equals(eight));
+    }
+
+    @Test
+    public void binaryStudy(){
+
+        Integer dos = new Integer(2);
+        Integer vint = new Integer("20");
+        Integer tretze = 13;
+
+
+        String tipusPersona = "Hi ha " + Integer.toBinaryString(dos) + " de personas, les que entenen binari i les que no ";
+
+        assertEquals(tipusPersona, 2 , dos.intValue());
+
+        //assertEquals(" El nombre vint val realmente 20 ", 20 , vint.intValue());
+    }
+
+}
